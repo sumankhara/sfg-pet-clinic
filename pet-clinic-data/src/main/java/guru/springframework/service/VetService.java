070035fefe3e9284ff1572df0b0293f54e8be36e
Suman Khara <suman.khara@cerner.com>
2021-08-25 20:08:44 +0530

    Add service interfaces for models

---
+++ pet-clinic-data/src/main/java/guru/springframework/service/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.service;
+
+import guru.springframework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
